425355f6cf2406e8a65f84384c4ffc32eca168dd
Sunjay E. Dixit <jaydixit.work@gmail.com>
2015-11-05 16:36:22 -0500

    gonna update my template

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
   (setq-default
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (ie. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '("/Users/jay/starship/jay")
+;;   dotspacemacs-configuration-layer-path '("/Users/jay/starship/jay")
 ;;   dotspacemacs-configuration-layer-path '("~/Dropbox/emacs/spacemacs/private/jay/")
    ;; List of configuration layers to load. If it is the symbol `all' instead
    ;; of a list then all discovered layers will be installed.
